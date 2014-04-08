6d3d204e4b9b2d793f9bd28e4f265dbf139ce87f
Christina Thompson <c.thompsonms@gmail.com>
2014-04-08 09:46:21 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySpiffyProject::Application.config.secret_key_base = '0908d0dc94d644c73239c9efa1abbaba5b19153e09b0b4ff66e6c319e05e5c69068690a9397fe44ef090a97c323784b336b9e012095227769b6a196eab6e5897'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MySpiffyProject::Application.config.session_store :cookie_store, key: '_my-spiffy-project_session'
